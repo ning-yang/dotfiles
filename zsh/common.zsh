2670de1ca1979cfb710ba13b83d42743b7dba08c
Ning Yang <csyangning@gmail.com>
2016-05-06 21:29:10 +0000

    Add ZSH configs and alias

---
+++ zsh/common.zsh
@@ -0,0 +1,9 @@
+# Alias Editing
+alias ae='vim ~/.alias/common.zsh'
+alias ar='source ~/.zshrc'
+
+# vimrc editing
+alias ve='vim ~/.vimrc'
+
+# clear
+alias cl='clear'
---
+++ zsh/skytap.zsh
@@ -0,0 +1,10 @@
+alias sql='mysql -h mysql0'
+alias charon='pushd ~/hosting_platform/hosting_platform/services/charon'
+alias account='pushd ~/hosting_platform/hosting_platform/services/accounting'
+alias greenbox='pushd ~/hosting_platform/hosting_platform/services/greenbox'
+alias cm='pushd ~/hosting_platform/hosting_platform/services/configuration_manager'
+
+# python
+alias p='python'
+alias pm='p -m pudb'
+alias purge='hg st -un0 | xargs -0 rm'
