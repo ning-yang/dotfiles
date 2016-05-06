28277c634f7bcad7a09d075cc5c160c74b262b5a
Ning Yang <csyangning@gmail.com>
2016-05-06 22:20:05 +0000

    add openssh package for remove access

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,6 +5,7 @@ apt-get update
 apt-get install -y vim vim-nox git zsh tmux 
 apt-get install -y build-essential cmake
 apt-get install -y python2.7 ipython python-pip python-dev 
+apt-get install -y openssh-server openssh-client
 
 # for mac: brew install the_silver_searcher
 apt-get install silversearcher-ag
