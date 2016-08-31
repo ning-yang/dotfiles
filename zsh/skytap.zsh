19d947f45138d9f21184172a48c5cecfcb9c5ee2
Ning Yang <csyangning@gmail.com>
2016-08-31 21:29:54 +0000

    Add nexus path

--- zsh/skytap.zsh
+++ zsh/skytap.zsh
@@ -3,6 +3,7 @@ alias charon='pushd ~/hosting_platform/hosting_platform/services/charon'
 alias account='pushd ~/hosting_platform/hosting_platform/services/accounting'
 alias greenbox='pushd ~/hosting_platform/hosting_platform/services/greenbox'
 alias cm='pushd ~/hosting_platform/hosting_platform/services/configuration_manager'
+alias nexus='pushd ~/hosting_platform/hosting_platform/services/nexus'
 
 # python
 alias p='python'
