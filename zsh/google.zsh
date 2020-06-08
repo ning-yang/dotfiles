ef9024968768bc059448892df4b0c2e1373284c1
Ning Yang <csyangning@gmail.com>
2020-06-07 23:45:43 -0700

    fix some zsh config

--- zsh/google.zsh
+++ zsh/google.zsh
@@ -1,11 +1,12 @@
-source /etc/bash_completion.d/g4d
+if [[ -f /etc/bash_completion.d/g4d ]]; then
+  source /etc/bash_completion.d/g4d
 
-eval "$(echo "_orig_git_prompt_info() {"; declare -f git_prompt_info | tail -n +2)"
-git_prompt_info() {
-  if [[ "$(p4 --format %Client% client -o)" =~ ".+:(.+):.+:.+" ]]; then
-    echo "g4:(%{$fg[red]%}$match%{$reset_color%})"
-  else
-    _orig_git_prompt_info
-  fi
-}
-
+  eval "$(echo "_orig_git_prompt_info() {"; declare -f git_prompt_info | tail -n +2)"
+  git_prompt_info() {
+    if [[ "$(p4 --format %Client% client -o)" =~ ".+:(.+):.+:.+" ]]; then
+      echo "g4:(%{$fg[red]%}$match%{$reset_color%})"
+    else
+      _orig_git_prompt_info
+    fi
+  }
+fi
--- zsh/virtualenvwrapper.zsh
+++ zsh/virtualenvwrapper.zsh
@@ -1,3 +1,3 @@
 export WORKON_HOME=$HOME/.virtualenvs
 export PROJECT_HOME=$HOME/projects
-source /usr/local/bin/virtualenvwrapper.sh
+# source /usr/local/bin/virtualenvwrapper.sh
