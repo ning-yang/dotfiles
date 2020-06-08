f251611c776c30465301e86296b58c5fb3fabe24
Ning <csyangning@gmail.com>
2020-06-08 09:28:40 -0700

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -27,7 +27,7 @@ ZSH_CUSTOM=~/dotfiles/zsh
 # zsh plugin and theme
 git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
 ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
-git clone https://github.com/zsh-users/zsh-autosuggestions "$ZSH_CUSTOM/plugins"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 # download dotfiles repo
